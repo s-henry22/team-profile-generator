35619ad7608005094b3ff15aa9af32aaab7d0396
Spencer Henry <spencerhenry94@gmail.com>
2021-04-28 22:22:44 -0400

    created a template literal for the HTML

--- app.js
+++ app.js
@@ -1,6 +1,41 @@
 const teamDataArgs = process.argv.slice(2, process.argv.length);
+const [name, title] = teamDataArgs;
 
-const printTeamData = teamDataArr => {
+const generatePage = (employeeName, jobTitle) => {
+    return `
+    <!DOCTYPE html> 
+    <html lang="en"> 
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Portfolio Demo</title>
+    </head>
+
+    <body>
+        <h1>${name}</h1>
+        <h2>${title}</h2>
+    </body>
+    </html>
+  `;
+};
+
+console.log(name, title);
+console.log(generatePage(name, title));
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const printTeamData = teamDataArr => {
     for (let i = 0; i < teamDataArr.length; i += 1) {
         console.log(teamDataArr[i]);
     }
@@ -12,4 +47,4 @@ const printTeamData = teamDataArr => {
     teamDataArr.forEach(teamItem => console.log(teamItem));
 };
 
-printTeamData(teamDataArgs);
+printTeamData(teamDataArgs);*/
