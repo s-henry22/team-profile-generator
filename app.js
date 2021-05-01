3d27eaf6facecadac580f93228ac8577eddac1a4
Spencer Henry <spencerhenry94@gmail.com>
2021-05-01 12:51:41 -0400

    installed inquirer

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
+/*const fs = require('fs');
 const generatePage = require('./src/page-template.js');
 
 const teamDataArgs = process.argv.slice(2, process.argv.length);
@@ -9,13 +10,13 @@ fs.writeFile('./index.html', generatePage(name, title), err => {
     if(err) throw err;
 
     console.log('Page complete!  Check out index.html to see the output');
-});
+});*/
 
-
-
-
-
-
-
-
-
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    }
+])
+.then(answers => console.log(answers));
